ab7977355a9c6281f8f2c59a7852eea413ff5f6f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-04 19:36:33 -0300

    push1

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,17 +11,12 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "tf-notifier-state-v1"
-    key            = "terraform.tfstate"
-    dynamodb_table = "tf-notifier-state-v1"
+    bucket         = "tf-notifier-state-v1-arcanjo"
+    key            = "terraform.tfstate-arcanjo"
+    dynamodb_table = "tf-notifier-state-v1-arcanjo"
     region         = "us-east-1"
   }
 
 }
 
-# provider "aws" {
-#   region                   = "us-east-1"
-#   shared_config_files      = ["./.aws/config"]
-#   shared_credentials_files = ["./.aws/credentials"]
-#   profile                  = "fiap"
-# }
+

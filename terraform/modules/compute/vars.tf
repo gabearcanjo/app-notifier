49209d2d8fa501262e787c888d7060f190087c71
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-04 20:30:42 -0300

    tent2

--- terraform/modules/compute/vars.tf
+++ terraform/modules/compute/vars.tf
@@ -26,8 +26,8 @@ variable "ec2_lt_ami" {
     type    = string
     default = "ami-02e136e904f3da870"
     validation {
-        condition     = length var.ec2_lt_ami > 4 && substr var.ec2_lt_ami,
-        error message = "The ami value must be a valid AMI id,
+        condition     = length var.ec2_lt_ami > 4 && substr var.ec2_lt_ami
+        error message = "The ami value must be a valid AMI id"
     }
 }
 
